225c31cb5bbf50ad031aeafd273771e67b87df97
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2020-06-11 22:08:58 -0300

    Add Metric and Plot Functions to All Algorithms

--- Segundo Trabalho - Redes Neurais/iris_classifier/iris_classifier.py
+++ Segundo Trabalho - Redes Neurais/iris_classifier/iris_classifier.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, mean_squared_error
 
 
+# Classe responsável por extrair os dados do csv e gerar um DataFrame Pandas com os Resultados
 class IrisDataExtractor:
     def __init__(self):
         self.dataset = pd.read_csv(self.data_url, header=None)
@@ -15,9 +18,12 @@ class IrisDataExtractor:
 
 
 class IrisPerceptron:
-    def __init__(self, number_of_attributes, class_labels):
-        self.weights = np.zeros(number_of_attributes + 1)
+    def __init__(self, number_of_attributes, class_labels, innitial_weight, innitial_alpha):
+        self.weights = [innitial_alpha]
+        self.weights = np.append(self.weights, [innitial_weight for _ in range(number_of_attributes)], axis=0)
+
         self.misclassify_record = []
+        self.squared_errror = []
 
         self._label_map = {
             1: class_labels[0],
@@ -46,10 +52,20 @@ class IrisPerceptron:
 
                 misclassifies += int(update != 0.0)
 
-            if misclassifies == 0:
-                break
+            # if misclassifies == 0:
+            #     break
 
             self.misclassify_record.append(misclassifies)
+
+            self.calculate_squared_error(samples, labels)
+
+    def calculate_squared_error(self, samples, labels):
+        transferred_labels = [self._reversed_label_map[index] for index in labels]
+
+        actual_predict = self.classify(samples)
+        transferred_actual_predict = [self._reversed_label_map[index] for index in actual_predict]
+
+        self.squared_errror.append(mean_squared_error(transferred_labels, transferred_actual_predict))
 
     def classify(self, new_data):
         predicted_result = np.where((self._linear_combination(new_data) + self.weights[0]) >= 0.0, 1, -1)
@@ -57,10 +73,10 @@ class IrisPerceptron:
         return [self._label_map[item] for item in predicted_result]
 
 
+
+#Inicio da análise de dados
 extractor = IrisDataExtractor()
 IRIS_DATA = extractor.get_dataset()
-
-print(IRIS_DATA)
 
 SETOSA_LABEL = IRIS_DATA.iloc[0:40, 4].values
 VERSICOLOR_LABEL = IRIS_DATA.iloc[50:90, 4].values
@@ -70,23 +86,24 @@ SETOSA_VERSICOLOR_TRAINING_LABEL = np.append(SETOSA_LABEL, VERSICOLOR_LABEL)
 SETOSA_VIRGINICA_TRAINING_LABEL = np.append(SETOSA_LABEL, VIRGINICA_LABEL)
 VERSICOLOR_VIRGINICA_TRAINING_LABEL = np.append(VERSICOLOR_LABEL, VIRGINICA_LABEL)
 
-# In this example, it uses only Sepal width and Petal width to train.
+
 SETOSA_DATA = IRIS_DATA.iloc[0:40, [0, 1, 2, 3]].values
 VERSICOLOR_DATA = IRIS_DATA.iloc[50:90, [0, 1, 2, 3]].values
 VIRGINICA_DATA = IRIS_DATA.iloc[100:140, [0, 1, 2, 3]].values
 
+#Criação das amostras de treinamento
 SETOSA_VERSICOLOR_TRAINING_DATA = np.append(SETOSA_DATA, VERSICOLOR_DATA, axis=0)
 SETOSA_VIRGINICA_TRAINING_DATA = np.append(SETOSA_DATA, VIRGINICA_DATA, axis=0)
 VERSICOLOR_VIRGINICA_TRAINING_DATA = np.append(VERSICOLOR_DATA, VIRGINICA_DATA, axis=0)
 
-# Prepare test data set. Use only Sepal width and Petal width as well.
+#Ceiação das amostras de teste
 SETOSA_TEST = IRIS_DATA.iloc[40:50, [0, 1, 2, 3]].values
 VERSICOLOR_TEST = IRIS_DATA.iloc[90:100, [0, 1, 2, 3]].values
 VIRGINICA_TEST = IRIS_DATA.iloc[140:150, [0, 1, 2, 3]].values
 TEST = np.append(SETOSA_TEST, VERSICOLOR_TEST, axis=0)
 TEST = np.append(TEST, VIRGINICA_TEST, axis=0)
 
-# Prepare the target of test data to verify the prediction
+#Criação das amostras de validação
 SETOSA_VERIFY = IRIS_DATA.iloc[40:50, 4].values
 VERSICOLOR_VERIFY = IRIS_DATA.iloc[90:100, 4].values
 VIRGINICA_VERIFY = IRIS_DATA.iloc[140:150, 4].values
@@ -94,24 +111,27 @@ VERIFY = np.append(SETOSA_VERIFY, VERSICOLOR_VERIFY)
 VERIFY = np.append(VERIFY, VIRGINICA_VERIFY)
 
 if __name__ == '__main__':
-    # Define a setosa-versicolor Perceptron() with 2 attributes
-    perceptron_setosa_versicolor = IrisPerceptron(number_of_attributes=4,
-                                                  class_labels=('Iris-setosa', 'Iris-versicolor'))
-    # Train the model
+    # Instancia o Primeiro Perceptron
+    perceptron_setosa_versicolor = IrisPerceptron(number_of_attributes=2,
+                                                  class_labels=('Iris-setosa', 'Iris-versicolor'), innitial_alpha=0.0,
+                                                  innitial_weight=0.0)
+    # Treina o modelo
     perceptron_setosa_versicolor.train(SETOSA_VERSICOLOR_TRAINING_DATA, SETOSA_VERSICOLOR_TRAINING_LABEL)
 
-    # Define a setosa-virginica Perceptron() with 2 attributes
-    perceptron_setosa_virginica = IrisPerceptron(number_of_attributes=4, class_labels=('Iris-setosa', 'Iris-virginica'))
-    # Train the model
+    # Instancia o Segundo Perceptron
+    perceptron_setosa_virginica = IrisPerceptron(number_of_attributes=2, class_labels=('Iris-setosa', 'Iris-virginica'),
+                                                 innitial_alpha=0.0, innitial_weight=0.0)
+    # Treina o Modelo
     perceptron_setosa_virginica.train(SETOSA_VIRGINICA_TRAINING_DATA, SETOSA_VIRGINICA_TRAINING_LABEL)
 
-    # Define a versicolor-virginica Perceptron() with 2 attributes
-    perceptron_versicolor_virginica = IrisPerceptron(number_of_attributes=4,
-                                                     class_labels=('Iris-versicolor', 'Iris-virginica'))
-    # Train the model
+    # Instancia o Terceiro Perceptron
+    perceptron_versicolor_virginica = IrisPerceptron(number_of_attributes=2,
+                                                     class_labels=('Iris-versicolor', 'Iris-virginica'),
+                                                     innitial_alpha=0.0, innitial_weight=0.0)
+    # Treina o modelo
     perceptron_versicolor_virginica.train(VERSICOLOR_VIRGINICA_TRAINING_DATA, VERSICOLOR_VIRGINICA_TRAINING_LABEL)
 
-    # Run three binary classifiers
+    # Executa todos os classificadores
     predict_target_1 = perceptron_setosa_versicolor.classify(TEST)
     predict_target_2 = perceptron_setosa_virginica.classify(TEST)
     predict_target_3 = perceptron_versicolor_virginica.classify(TEST)
@@ -120,15 +140,33 @@ if __name__ == '__main__':
     for item in zip(predict_target_1, predict_target_2, predict_target_3):
         unique, counts = np.unique(item, return_counts=True)
         temp_result = (zip(unique, counts))
-        # Sort by values and return the class that has majority votes
+        # Ordena pelos valores e seleciona os mais "votados"
         overall_predict_result.append(
             sorted(temp_result, reverse=True, key=lambda tup: tup[1])[0][0])
 
     print(overall_predict_result)
 
-    # Verify the results
+    # Confere os resultados (estava sendo utlizado antes de inserir os auxiliares da sklearn
     misclassified = 0
     for predict, verify in zip(overall_predict_result, VERIFY):
         if predict != verify:
             misclassified += 1
     print("The number of misclassified: " + str(misclassified))
+
+    print("Accuracy score: " + str(accuracy_score(VERIFY, overall_predict_result)))
+    print("\nConfusion matrix: \n" + str(confusion_matrix(VERIFY, overall_predict_result)))
+    print("\nClassification report: \n" + str(classification_report(VERIFY, overall_predict_result)))
+
+    plt.plot(perceptron_setosa_versicolor.squared_errror, color='blue', label='Erro Quadrático Setosa Versicolor')
+    plt.plot(perceptron_versicolor_virginica.squared_errror, color='green',
+             label='Erro Quadrático Virginica Versicolor')
+    plt.plot(perceptron_setosa_virginica.squared_errror, color='red', label='Erro Quadrático Setosa Virginica')
+
+    plt.ylabel('Erro Médio Quadrático por Épocas')
+    plt.xlabel('Épocas')
+    plt.title('Erro Médio Quadrático')
+
+    plt.legend(loc='upper right')
+    plt.grid(True)
+
+    plt.show()
--- Segundo Trabalho - Redes Neurais/wine_classifier/wine_classifier.py
+++ Segundo Trabalho - Redes Neurais/wine_classifier/wine_classifier.py
@@ -1,9 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
+# Classe responsável por extrair os dados do csv e gerar um DataFrame Pandas com os Resultados
 class WineDataExtractor:
     def __init__(self):
         self.dataset = pd.read_csv(self.data_url, header=None)
@@ -17,9 +20,12 @@ class WineDataExtractor:
 
 
 class WinePerceptron:
-    def __init__(self, number_of_attributes, class_labels):
-        self.weights = np.zeros(number_of_attributes + 1)
+    def __init__(self, number_of_attributes, class_labels, innitial_weight, innitial_alpha):
+        self.weights = [innitial_alpha]
+        self.weights = np.append(self.weights, [innitial_weight for _ in range(number_of_attributes)], axis=0)
+
         self.misclassify_record = []
+        self.squared_errror = []
 
         self._label_map = {
             1: class_labels[0],
@@ -48,10 +54,20 @@ class WinePerceptron:
 
                 misclassifies += int(update != 0.0)
 
-            if misclassifies == 0:
-                break
+            # if misclassifies == 0:
+            #     break
 
             self.misclassify_record.append(misclassifies)
+
+            self.calculate_squared_error(samples, labels)
+
+    def calculate_squared_error(self, samples, labels):
+        transferred_labels = [self._reversed_label_map[index] for index in labels]
+
+        actual_predict = self.classify(samples)
+        transferred_actual_predict = [self._reversed_label_map[index] for index in actual_predict]
+
+        self.squared_errror.append(mean_squared_error(transferred_labels, transferred_actual_predict))
 
     def classify(self, new_data):
         predicted_result = np.where((self._linear_combination(new_data) + self.weights[0]) >= 0.0, 1, -1)
@@ -59,20 +75,22 @@ class WinePerceptron:
         return [self._label_map[item] for item in predicted_result]
 
 
+#inicio da analise de dados
 extractor = WineDataExtractor()
 WINE_DATA = extractor.get_dataset()
-WINE_DATA.columns = ['Label', 'Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of ash ', 'Magnesium', 'Total phenols', 'Flavanoids' , 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
-
 
 if __name__ == '__main__':
     WINE_LABELS = WINE_DATA.values[:, 0]
     WINE_DATA_VALUES = WINE_DATA.values[:, 1:13]
 
+    #normaliza os modelos
     standard_scaller = StandardScaler()
     standard_scaller.fit(WINE_DATA_VALUES)
 
+    #Normaliza todos os valores
     WINE_DATA_VALUES = standard_scaller.transform(WINE_DATA_VALUES)
 
+    # Seleciona as amostras por classe
     FIRST_CLASS_LABELS = [str(item) for item in WINE_LABELS[0:58]]
     FIRST_CLASS_VALUES = WINE_DATA_VALUES[0:58]
 
@@ -82,7 +100,9 @@ if __name__ == '__main__':
     THIRD_CLASS_LABELS = [str(item) for item in WINE_LABELS[130:]]
     THIRD_CLASS_VALUES = WINE_DATA_VALUES[130:]
 
-    first_class_train_values, first_class_test_values, first_class_train_labels, first_class_test_labels = train_test_split(FIRST_CLASS_VALUES, FIRST_CLASS_LABELS, test_size=0.2, random_state=1)
+    # divide os grupos de treino e teste
+    first_class_train_values, first_class_test_values, first_class_train_labels, first_class_test_labels = train_test_split(
+        FIRST_CLASS_VALUES, FIRST_CLASS_LABELS, test_size=0.2, random_state=1)
 
     second_class_train_values, second_class_test_values, second_class_train_labels, second_class_test_labels = train_test_split(
         SECOND_CLASS_VALUES, SECOND_CLASS_LABELS, test_size=0.2, random_state=1)
@@ -90,6 +110,7 @@ if __name__ == '__main__':
     third_class_train_values, third_class_test_values, third_class_train_labels, third_class_test_labels = train_test_split(
         THIRD_CLASS_VALUES, THIRD_CLASS_LABELS, test_size=0.2, random_state=1)
 
+    # cria efetivamente os grupos a serem analisados
     first_class_second_class_training_values = np.append(first_class_train_values, second_class_train_values, axis=0)
     first_class_second_class_labels = np.append(first_class_train_labels, second_class_train_labels, axis=0)
 
@@ -99,20 +120,28 @@ if __name__ == '__main__':
     first_class_third_class_training_values = np.append(first_class_train_values, third_class_train_values, axis=0)
     first_class_third_class_training_labels = np.append(first_class_train_labels, third_class_train_labels, axis=0)
 
-    first_class_second_class_classifier = WinePerceptron(number_of_attributes=12, class_labels=['1.0', '2.0'])
+    # instancia os Perceptrons e treina-os
+    first_class_second_class_classifier = WinePerceptron(number_of_attributes=12, class_labels=['1.0', '2.0'],
+                                                         innitial_alpha=10.0, innitial_weight=0.0)
     first_class_second_class_classifier.train(first_class_second_class_training_values, first_class_second_class_labels)
 
-    second_class_third_class_classifier = WinePerceptron(number_of_attributes=12, class_labels=['2.0', '3.0'])
-    second_class_third_class_classifier.train(second_class_third_class_training_values, second_class_third_class_training_labels)
+    second_class_third_class_classifier = WinePerceptron(number_of_attributes=12, class_labels=['2.0', '3.0'],
+                                                         innitial_alpha=200.0, innitial_weight=16000)
+    second_class_third_class_classifier.train(second_class_third_class_training_values,
+                                              second_class_third_class_training_labels)
 
-    first_class_third_class_classifier = WinePerceptron(number_of_attributes=12, class_labels=['1.0', '3.0'])
-    first_class_third_class_classifier.train(first_class_third_class_training_values, first_class_third_class_training_labels)
+    first_class_third_class_classifier = WinePerceptron(number_of_attributes=12, class_labels=['1.0', '3.0'],
+                                                        innitial_alpha=0.0, innitial_weight=1.0)
+    first_class_third_class_classifier.train(first_class_third_class_training_values,
+                                             first_class_third_class_training_labels)
 
+    # prepara o grupo de teste e verificação
     test_data = np.append(first_class_test_values, second_class_test_values, axis=0)
     test_data = np.append(test_data, third_class_test_values, axis=0)
     test_label = np.append(first_class_test_labels, second_class_test_labels, axis=0)
     test_label = np.append(test_label, third_class_test_labels, axis=0)
 
+    # obtem os valores classificados
     first_predict_target = first_class_second_class_classifier.classify(test_data)
     second_predict_target = second_class_third_class_classifier.classify(test_data)
     third_predict_target = first_class_third_class_classifier.classify(test_data)
@@ -121,14 +150,38 @@ if __name__ == '__main__':
     for item in zip(first_predict_target, second_predict_target, third_predict_target):
         unique, counts = np.unique(item, return_counts=True)
         temp_result = (zip(unique, counts))
-        # Sort by values and return the class that has majority votes
+        # sorteia por valor e seleciona "os mais votados"
         overall_predict_result.append(
             sorted(temp_result, reverse=True, key=lambda tup: tup[1])[0][0])
 
+
+    # exibe a quantidade de valores classificados erroneamente
     misclassified = 0
     for predict, verify in zip(overall_predict_result, test_label):
         if predict != verify:
             misclassified += 1
 
     print(len(test_data))
-    print("The number of misclassified: " + str(misclassified))
+    print(f"The number of misclassified: {misclassified}")
+
+    print(f"Accuracy score: {accuracy_score(test_label, overall_predict_result)}")
+    print("Confusion matrix: \n")
+    print(confusion_matrix(test_label, overall_predict_result))
+    print("Classification report:")
+    print(classification_report(test_label, overall_predict_result))
+
+    plt.plot(first_class_second_class_classifier.squared_errror, color='blue',
+             label='Erro Quadrático Primeira Classe Segunda Classe')
+    plt.plot(first_class_third_class_classifier.squared_errror, color='green',
+             label='Erro Quadrático Primeira Classe Terceira Classe')
+    plt.plot(second_class_third_class_classifier.squared_errror, color='red',
+             label='Erro Quadrático Segunda Classe Terceira Classe')
+
+    plt.ylabel('Erro Médio Quadrático por Épocas')
+    plt.xlabel('Épocas')
+    plt.title('Erro Médio Quadrático')
+
+    plt.legend(loc='upper right')
+    plt.grid(True)
+
+    plt.show()
